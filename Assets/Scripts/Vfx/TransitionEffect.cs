e0c9b30c6fe08a23d03af578af1151e7842748f6
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2026-04-16 11:34:44 +0700

    fix: make transition effect singleton too because it duplicated itself

--- Assets/Scripts/Vfx/TransitionEffect.cs
+++ Assets/Scripts/Vfx/TransitionEffect.cs
@@ -2,8 +2,21 @@ using UnityEngine;
 
 public class TransitionEffect : MonoBehaviour
 {
+    
+    private static TransitionEffect instance;
+    public static TransitionEffect Instance => instance;
+
+
     private void Awake()
     {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+
         DontDestroyOnLoad(gameObject);
     }
 }
