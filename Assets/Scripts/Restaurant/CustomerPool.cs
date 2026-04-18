254b9807039e52669274bca17662b1979f2efe69
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2026-04-18 16:39:22 +0700

    chore: create customer pool

---
+++ Assets/Scripts/Restaurant/CustomerPool.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerPool : MonoBehaviour
+{
+    [SerializeField] private Customer customerPrefab;
+    [SerializeField] private int amountOfCustomer = 100;
+
+    private List<Customer> customers;
+
+    private void Awake()
+    {
+        customers = new List<Customer>();
+
+        for (int i = 0; i < amountOfCustomer; i++)
+        {
+            Customer c = Instantiate(customerPrefab, transform);
+            c.gameObject.SetActive(false);
+            customers.Add(c);
+        }
+    }
+
+    private Customer GetCustomer()
+    {
+        Customer customer = customers[customers.Count - 1];
+
+        customers.RemoveAt(customers.Count - 1);
+
+        return customer;
+    }
+
+    private void ReturnCustomer(Customer c)
+    {
+        customers.Add(c);
+    }
+}
