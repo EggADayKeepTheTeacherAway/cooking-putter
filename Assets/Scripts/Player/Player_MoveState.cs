bf6e497d372479c46cf73733929924b13da3f782
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2026-05-08 13:39:28 +0700

    chore: increase player animation speed when running

--- Assets/Scripts/Player/Player_MoveState.cs
+++ Assets/Scripts/Player/Player_MoveState.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Player_MoveState : PlayerState
 {
+    private float walkingAnimationSpeed = 1f;
+    private float runningAnimationSpeed = 2.5f;
+
+
     public Player_MoveState(Player player, StateMachine stateMachine, string animParam) : base(player, stateMachine, animParam)
     {
     }
@@ -17,15 +21,25 @@ public class Player_MoveState : PlayerState
         
         if (player.input.Player.Sprint.IsPressed())
         {
+            anim.SetFloat("speedMultiplier", runningAnimationSpeed);
             player.SetVelocity(
                 player.moveInput.x * player.moveSpeed * player.runSpeedModifier,
                 player.moveInput.y * player.moveSpeed * player.runSpeedModifier
             );
+
         }
 
         else
         {
+            anim.SetFloat("speedMultiplier", walkingAnimationSpeed);
             player.SetVelocity(player.moveInput.x * player.moveSpeed, player.moveInput.y * player.moveSpeed);
         }
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+
+        anim.SetFloat("speedMultiplier", walkingAnimationSpeed);
     }
 }
